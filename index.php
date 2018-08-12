2a40009431e04d0e0b566a147f3735166a3e052b
Pavel Batishchev <batiskaf.vrn@yandex.ru>
2018-08-12 14:35:44 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Тестовая страница</title>
+</head>
+<body>
+<?php 
+    $name = "Паша";
+    $age = 24;
+    echo "Меня зовут " . $name;
+    echo "<p>";
+    echo "Мне " . $age . " года";
+?>
+</body>
+</html>
